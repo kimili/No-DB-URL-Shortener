067033ab11747a09915bac07436f485389bb2cc2
Michael Bester <michael@kimili.com>
2014-01-12 22:37:54 -0500

    Code cleanup and refactor
    - Refactoring code into a class URLShortener
    - Checking for existing hashes so as not to overwrite them
    - Added the ability to pass in a numeric ID to shorten
    - Added a separate URL endpoint for the creation process
    - Tweaked the auto ID generation to result in shorter hashes.

--- index.php
+++ index.php
@@ -1,163 +1,7 @@
 <?php
-
-/*
-
-	No DB URL Shortener
-	(c) 2014 Michael Bester
-	https://github.com/kimili/No-DB-URL-Shortener
-	This code may be freely distributed under the MIT license.
-
-	Props to Ryan Petrich for the idea (https://gist.github.com/rpetrich/627137)
-	Thanks to Ivan Akimov for the awesome HashIds library (http://www.hashids.org/php/)
-	Thanks to Solar Designs at Openwall for PHPass (http://www.openwall.com/phpass/)
-
-*/
-
-define('__ROOT__', dirname(__FILE__));
-
-/*
- * Load required libraries
- */
-require_once(__ROOT__.'/lib/PasswordHash/PasswordHash.php');
-require_once(__ROOT__.'/lib/Hashids/Hashids.php');
-
-/*
- * Get the config
- */
-
-define('CONFIG_FILE', __ROOT__.'/inc/config.php');
-if ( ! file_exists(CONFIG_FILE) ) {
-	die("Can't find the configuration file. Please make sure it is set up.");
-}
-require_once(CONFIG_FILE);
-
-// Check for the hashed password
-if ( ! defined('HASHED_PASSWORD')  ) {
-	// If we don't have it, we have to generate it from the config
-	// Let's see if we need to display the set password form.
-	if ( ! isset($_POST['newpassword']) ) {
-		$setPassword = true;
-	} else {
-		$hasher = new PasswordHash(32768, false);
-		$hash = $hasher->HashPassword($_POST['newpassword']);
-
-		// Open up the config file for writing the hash to
-		$handle = fopen(CONFIG_FILE, 'a') or die('Cannot open file: ' . CONFIG_FILE);
-
-		// Set up the new data to write to the config.
-		$configAdditions = array();
-		$configAdditions[] = '';
-		$configAdditions[] = '// This hashed password is set automatically.';
-		$configAdditions[] = '// To reset your password, delete this comment and the HASHED_PASSWORD line below reload index.php in a browser';
-		$configAdditions[] = "define('HASHED_PASSWORD', '$hash');";
-
-		// And write it.
-		fwrite($handle, $configContents . implode("\n", $configAdditions));
-		// Close out the file
-		fclose($handle);
-	}
-}
-
-/*
- * A function to get params from either get or post requests.
- */
-function param($key = null) {
-	if ( $key == null ) {
-		return '';
-	}
-	switch ($_SERVER['REQUEST_METHOD']) {
-		case 'POST':
-			return $_POST[$key];
-			break;
-		default:
-			return $_GET[$key];
-			break;
-	}
-}
-
-/*
- * Set some parameters we'll need
- */
-$url = $_SERVER['REQUEST_URI'];
-$contentDir = 'content/';
-
-/*
- * Check our parameters to see if we want to write a new short URL
- */
-if ( param('pw') ) {
-
-	// We're trying to set up a new link
-	// Let's spit out JSON
-	header('Content-type: application/json');
-	$output = new stdClass;
-
-	// Check the password
-	$hasher = new PasswordHash(32768, false);
-	$authCheck = $hasher->CheckPassword(param('pw'), HASHED_PASSWORD);
-
-	if ($authCheck) {
-		// If we're in here, we're authenticated
-
-		// Did we pass in a link?
-		if ( trim(param('link')) != '' ) {
-			$slug = trim(param('slug'));
-			if ( $slug != '' ) {
-				// Did we pass in a slug to use?
-				$hash = param('slug');
-			} else {
-				// if not, generate one.
-				// Alphabet excludes 0, O, I, and l to minimize ambiguious hashes
-				$alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
-				$hashids = new Hashids\Hashids($hashSalt, 1, $alphabet);
-
-				// Set the default timezone
-				date_default_timezone_set(TIMEZONE);
-
-				// get the current timestamp as a number that represents YYMMDDHHMMSS and use it as an ID
-				$id = intval(date('ymdHis'));
-				$hash = $hashids->encrypt($id);
-			}
-			$fh = fopen("$contentDir/urls/$hash.url", 'w') or die("Can't open file for writing. Please check your permissions");
-			fwrite($fh, param('link'));
-			fclose($fh);
-
-			$output->originalURL = param('link');
-			$output->shortURL = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . '/' . $hash;
-			$output->baseURL = $_SERVER['SERVER_NAME'];
-			$output->hash = $hash;
-
-			echo json_encode($output);
-			exit();
-		} else {
-			$output->error = "No link passed in to shorten.";
-			echo json_encode($output);
-			exit();
-		}
-	} else {
-		$output->error = "Sorry, but your password was incorrect.";
-		echo json_encode($output);
-		exit();
-	}
-}
-
-
-
-/*
- * Handle incoming Short URLs
- */
-if (strpos($url, '.') == false) {
-	$hash = substr($url, 1);
-	// Shortened URL
-	if (file_exists("$contentDir/urls/$hash.url")) {
-		$contents = file("$contentDir/urls/$hash.url");
-		header('HTTP/1.1 301 Moved Permanently');
-		header('Location: '.$contents[0]);
-		exit();
-	}
-}
-
+require_once __dir__ . '/lib/URLShortener/URLShortener.php';
+$URLShortener = new URLShortener();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -165,10 +9,10 @@ if (strpos($url, '.') == false) {
 	<title><?php echo ucfirst($_SERVER['SERVER_NAME']) ?></title>
 </head>
 <body>
-	<?php if ($setPassword): ?>
+	<?php if ($URLShortener->needs_password_set): ?>
 		<form action="." method="post" accept-charset="utf-8">
 			<label for="password">Set a password</label>
-			<input type="text" name="newpassword" value="" id="password">
+			<input type="password" name="newpassword" value="" id="password">
 			<input type="submit" value="Continue &rarr;">
 		</form>
 	<?php else: ?>
---
+++ lib/URLShortener/URLShortener.php
@@ -0,0 +1,326 @@
+<?php
+/*
+
+	No DB URL Shortener
+	(c) 2014 Michael Bester
+	https://github.com/kimili/No-DB-URL-Shortener
+	This code may be freely distributed under the MIT license.
+
+	Props to Ryan Petrich for the idea (https://gist.github.com/rpetrich/627137)
+	Thanks to Ivan Akimov for the awesome HashIds library (http://www.hashids.org/php/)
+	Thanks to Solar Designs at Openwall for PHPass (http://www.openwall.com/phpass/)
+
+*/
+
+
+/*
+ * Load required libraries
+ */
+require_once(realpath(__DIR__ . '/../PasswordHash/PasswordHash.php'));
+require_once(realpath(__DIR__ . '/../Hashids/Hashids.php'));
+
+define('CONFIG_FILE', realpath(__DIR__ . '/../../inc/config.php'));
+
+/*
+ * Get the config
+ */
+if ( ! file_exists(CONFIG_FILE) ) {
+	die("Can't find the configuration file. Please make sure it is set up.");
+}
+require_once(CONFIG_FILE);
+
+
+
+/**
+* URLShortener Class
+*/
+class URLShortener
+{
+
+	private $_content_dir = 'content/';
+	private $_daily_count_file = '';
+
+	// Alphabet excludes 0, O, I, and l to minimize ambiguious hashes
+	private $_alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+
+	public $needs_password_set = false;
+
+	function __construct()
+	{
+		// Set up the private variable values
+		$this->_daily_count_file = realpath(__DIR__ . '/../../inc/daily-count.txt');
+
+		// Off the bat, let's check to see if we have a hashed password
+		if ( ! $this->_is_hashed_password_defined() ) {
+			// It's up to the end user to implement a method (a form)
+			// which sets a password and allows us to save a salted hash of it.
+			if ( $this->_get_param('newpassword') == '' ) {
+				$this->needs_password_set = true;
+			} else {
+				// If we're here, we've passed in a new password to hash.
+				$this->_set_hashed_password();
+			}
+			return;
+		}
+
+		// Here, let's check to see if we're passing in a new link
+		if ( $_GET['create'] == 'true' ) {
+			// Let's put this in JSON mode
+			header('Content-type: application/json');
+			echo json_encode($this->_create_new_shortlink());
+			exit();
+		}
+
+		// Did we get here? See if we need to forward the user along to a URL
+		$this->_do_saved_link_check();
+	}
+
+	/**
+	 * Creates the new shortlink if the parameters are good
+	 *
+	 * @return void
+	 **/
+	private function _create_new_shortlink()
+	{
+
+		// First, check the password
+		$this->_init_password_hasher();
+		$authCheck = $this->_hasher->CheckPassword($this->_get_param('pw'), HASHED_PASSWORD);
+
+		if ( $authCheck ) {
+			// In here, we're authorized to create a new shortlink
+			// First, let's see if we have a specific hash that was passed in.
+			// We'll use it as long as we have it and it's unique
+			$hash = $this->_get_param('hash');
+			if ( ! $hash || ($hash != '' && $this->_does_hash_exist($hash)) ) {
+				// Did we get here? We need a new unique hash
+				$hash = $this->_generate_new_hash();
+			}
+			// Now that we have a hash, try to save the link.
+			return $this->_save_link($hash);
+		} else {
+			$output = new stdClass;
+			$output->error = "Sorry, but your password was incorrect.";
+			return $output;
+		}
+	}
+
+	/**
+	 * Sets the hashed password in the config file if one doesn't exist
+	 *
+	 * @return void
+	 **/
+	private function _set_hashed_password()
+	{
+		$newpassword = $this->_get_param('newpassword');
+
+		if ( $this->_is_hashed_password_defined() || $newpassword == '' ) {
+			return;
+		}
+
+		// Set up a new hasher instance
+		$this->_init_password_hasher();
+		$hash = $this->_hasher->HashPassword($newpassword);
+
+		// Open up the config file for writing the hash to
+		$handle = fopen(CONFIG_FILE, 'a') or die('Cannot open file: ' . CONFIG_FILE);
+
+		// Set up the new data to write to the config.
+		$configAdditions = array();
+		$configAdditions[] = '';
+		$configAdditions[] = '// This hashed password is set automatically.';
+		$configAdditions[] = '// To reset your password, delete this comment and the HASHED_PASSWORD line below reload index.php in a browser';
+		$configAdditions[] = "define('HASHED_PASSWORD', '$hash');";
+
+		// And write it.
+		fwrite($handle, implode("\n", $configAdditions));
+		// Close out the file
+		fclose($handle);
+	}
+
+	/**
+	 * Tells us whether or not the hashed password is defined
+	 *
+	 * @return void
+	 **/
+	private function _is_hashed_password_defined()
+	{
+		return defined('HASHED_PASSWORD');
+	}
+
+	/**
+	 * Checks to see if we have a saved link via the incoming hash and forwards the user if it's found
+	 *
+	 * @return void
+	 **/
+	private function _do_saved_link_check()
+	{
+		$url = $_SERVER['REQUEST_URI'];
+		if ( strpos($url, '.') == false ) {
+			$hash = substr($url, 1);
+			// Shortened URL
+			if (file_exists("$this->_content_dir/urls/$hash.url")) {
+				$contents = file("$this->_content_dir/urls/$hash.url");
+				header('HTTP/1.1 301 Moved Permanently');
+				header('Location: '.$contents[0]);
+				exit();
+			}
+		}
+	}
+
+	/**
+	 * Generates a new unique hash
+	 *
+	 * @return string - the new hash
+	 **/
+	private function _generate_new_hash()
+	{
+		// Set up a new instance of hashids
+		$hashids = new Hashids\Hashids(HASH_SALT, 1, $this->alphabet);
+
+		// Set the default timezone
+		date_default_timezone_set(TIMEZONE);
+
+		// Get the daily count
+		$dailycount = $this->_get_daily_count();
+
+		// Let's check if the user passed in an ID to hash
+		$passed_id = $this->_get_param('id');
+		if ( $passed_id ) {
+			$id = intval($passed_id . $dailycount);
+		} else {
+			// No id passed in?
+			// Get the current timestamp as a number that represents YYMMDD
+			// We'll append the daily count to it and use it as an ID
+			$datestr = date('ymd');
+			$id = intval($datestr . $dailycount);
+		}
+		// Generate the hash
+		$hash = $hashids->encrypt($id);
+
+		// Check to see if the hash is already in use
+		if ( $this->_does_hash_exist($hash) ) {
+			$this->_increment_daily_count();
+			$this->_generate_new_hash();
+		}
+
+		return $hash;
+	}
+
+	/**
+	 * Writes a new link file
+	 *
+	 * @param $hash - the to use to save it
+	 * @return Object - The link data, or an error if the file was not writable.
+	 **/
+	private function _save_link($hash)
+	{
+		$output = new stdClass;
+		$link = trim($this->_get_param('link'));
+
+		if ( $link ) {
+			$fh = fopen("$this->_content_dir/urls/$hash.url", 'w');
+			if ( $fh ) {
+				fwrite($fh, $link);
+				fclose($fh);
+
+				$output->originalURL = $link;
+				$output->shortURL = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . '/' . $hash;
+				$output->baseURL = $_SERVER['SERVER_NAME'];
+				$output->hash = $hash;
+				// Increment the daily count
+				$this->_increment_daily_count();
+			} else {
+				$output->error = "Could not save $link as a short URL. Please check your file permissions.";
+			}
+		} else {
+			$output->error = "No link passed in to shorten.";
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Checks to see if a hash already exists
+	 *
+	 * @param $hash - the hash string to check
+	 * @return boolean
+	 **/
+	private function _does_hash_exist($hash)
+	{
+		return file_exists("$this->_content_dir/urls/$hash.url");
+	}
+
+	/**
+	 * creates a new instance of the password hasher
+	 *
+	 * @return void
+	 **/
+	private function _init_password_hasher()
+	{
+		if ( isset($this->_hasher) ) {
+			return;
+		}
+		$this->_hasher = new PasswordHash(32768, false);
+	}
+
+	/**
+	 * Reads the value from the daily count file
+	 *
+	 * @return void
+	 **/
+	private function _get_daily_count()
+	{
+		return trim(file_get_contents($this->_daily_count_file));
+	}
+
+	/**
+	 * Updates the value of the daily count file
+	 *
+	 * @return void
+	 **/
+	private function _increment_daily_count()
+	{
+		$now = time();
+		$last_modified_time = filemtime($this->_daily_count_file);
+		$daily_seconds = 24 * 60 * 60;
+		$daily_count = $this->_get_daily_count();
+
+		// Was the last modified time of the daily count file today?
+		if ( $last_modified_time > ($now - ($now % $daily_seconds)) ) {
+			$daily_count = intval($daily_count) + 1;
+		} else {
+			// it was last modified before today - let's restart the count
+			$daily_count = 0;
+		}
+		// update the daily count file
+		$handle = fopen($this->_daily_count_file, 'w+') or die('Cannot open file: ' . $this->_daily_count_file);
+		fwrite($handle, $daily_count);
+		fclose($handle);
+	}
+
+	/**
+	 * A function to get params from either get or post requests.
+	 *
+	 * @param key - a parameter key to get
+	 * @return string - the parameter value, if found
+	 **/
+	private function _get_param($key = null)
+	{
+		if ( $key == null ) {
+			return '';
+		}
+		switch ($_SERVER['REQUEST_METHOD']) {
+			case 'POST':
+				return $_POST[$key];
+				break;
+			default:
+				return $_GET[$key];
+				break;
+		}
+	}
+
+}
+
+
+?>
